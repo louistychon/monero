4ffdd73c8213397f2ade76f2535a3590bfbd9d17
louis.tychon1@gmail.com <louis.tychon1@gmail.com>
2022-03-16 11:04:37 +0100

    first commits on test branch

---
+++ public/core/header.php
@@ -0,0 +1,13 @@
+<header>
+        <nav class="bg-white shadow dark:bg-gray-800">
+            <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+            <div class="logo">
+            <!--<img class="logo_monero_big" src="images/logo_monero.png">-->
+            <img class="logo_monero_small" src="images/logo_monero_small.png">
+            </div>
+                <a href="#" class="text-gray-800 transition-colors duration-200 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Accueil</a>
+
+                <a href="<?php $Server_root/pages/a-propos.php ?>" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">À propos</a>
+            </div>
+        </nav>
+    </header>
---
+++ public/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include 'core/header.php' ?>
+    <main>
+        <section>
+            <h1>Monero : l'explication simpe</h1>
+            <p>La majorité des cryptomonnaies aujourd'hui (incluant le Bitcoin) ne sont pas tout à fait transparentes. Bien que l'utilisateur soit caché derrière une adresse de portefeuille, il est possible de retrouver les détenteurs d'une adresse, et n'importe qui dans le monde peut donc retrouver le détenteur d'une adresse spécifique.</p>
+        </section>
+        <section>
+            <h2>En quoi le monero est-il différent du BTC ? </h2>
+            <p>Monero est la seule cryptomonnaie pour laquelle chaque utilisateur est anonyme par defaut. Cette confidentialité permanente signifie que l'activité de chaque utilisateur Monero améliore la confidentialité de tous les autres utilisateurs, contrairement aux cryptomonnaies à transparence sélective (par exemple le Zcash). </p>
+        </section>
+        <section>
+            <h2>Le monero est-il vraiment anonyme ?</h2>
+            <p>Il n'y a pas de frais cachés et aucun risque de rétrofacturation frauduleuse. Monero est à l'abri des « contrôles de capitaux » - qui sont des mesures limitant les flux de monnaies traditionnelles, parfois à un degré extrême, dans des pays en proie à une instabilité économique.</p>
+        </section>
+    </main>
+    <?php include 'core/footer.php';?>
+</body>
+
+</html>
