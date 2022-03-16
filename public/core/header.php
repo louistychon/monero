e5e4be72a7d797f59c40921c6d94a92838c3e2cd
louis.tychon1@gmail.com <louis.tychon1@gmail.com>
2022-03-16 14:30:50 +0100

    alignement page d'accueil

--- public/core/header.php
+++ public/core/header.php
@@ -5,7 +5,7 @@
             <!--<img class="logo_monero_big" src="images/logo_monero.png">-->
             <img class="logo_monero_small" src="images/logo_monero_small.png">
             </div>
-                <a href="#" class="text-gray-800 transition-colors duration-200 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Accueil</a>
+                <a href="../index.php" class="text-gray-800 transition-colors duration-200 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Accueil</a>
 
                 <a href="../pages/a-propos.php" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">À propos</a>
             </div>
--- public/pages/a-propos.php
+++ public/pages/a-propos.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link href="css/style.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
 </head>
 <body>
     <?php include '../core/header.php' ?>
